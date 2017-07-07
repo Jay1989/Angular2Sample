414f69d55489f9475e594a2df8835d0ce329219c
Jay1989 <jay.iitd@gmail.com>
2017-07-07 21:26:10 +0530

    2nd commit

--- src/app/component/about.component.ts
+++ src/app/component/about.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core'
 @Component({
     moduleId: module.id,
     selector: 'about-comp',
-    template:`<h3>I am in about Component test 123</h3>`
+    template:`<h3>I am in about Component test 12345</h3>`
 })
 export class AboutComponent {
 
